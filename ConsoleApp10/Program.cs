526ec92466fd181b97f8e064feed9efe93ac5408
fidannh <fidannh@code.edu.az>
2023-03-07 03:10:06 +0400

    task

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿User user = new User("username","password");
+
+
+Console.WriteLine("Please enter the username:");
+string UserName = Console.ReadLine();
+Console.WriteLine("Please enter the  password:");
+string Password = Console.ReadLine();
+
+if(user.UserName != null && user.Password != null)
+{
+    Console.WriteLine("Completed successfully");
+}
+else
+{
+    Console.WriteLine("Incorrect ");
+}
---
+++ ConsoleApp10/User.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+internal class User
+{
+    private string _username;
+    private string _password;
+
+    public string UserName
+    {
+        get
+        {
+            return _username;
+        }
+        set
+        {
+            _username = value;
+        }
+    }
+
+    public string Password
+    {
+        get { return _password; }
+        set
+        {
+            if(CheckPassword(value)==true)
+            {
+                _password = value;
+            }
+        }
+    } 
+    public User(string password,string username)
+    {
+        this.Password = password;
+        this.UserName = username;
+    }
+    public bool CheckPassword(string Password)
+    {
+        bool status=false;
+        if(Password.Length>7 && Password.Any(char.IsUpper))
+        {
+            status = true;
+        }
+        else
+        {
+            Console.WriteLine("The password must contain 8 characters,digits and Uppercase character");
+        }
+        return status;
+
+    }
+
+}
+ 
+
+
